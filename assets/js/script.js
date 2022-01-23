d1bb9fdaa5d42dca473af79ea15df33f733bdc33
zk229 <zkirsche01@gmail.com>
2022-01-23 15:11:41 -0500

    add input validation and clear field on submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,12 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    if(!(taskNameInput && taskTypeInput)) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+    formEl.reset();
+
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
