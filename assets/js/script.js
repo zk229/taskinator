e3e70988929182519617f9d146a3515b238196e6
zk229 <zkirsche01@gmail.com>
2022-01-23 13:10:34 -0500

    add task type selector

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,18 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
     event.preventDefault();
 
-    var taskItemEl = document.createElement("li");
-    taskItemEl.textContent = "new task";
-    taskItemEl.className = "task-item";
-    tasksToDoEl.appendChild(taskItemEl);
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+    tasksToDoEl.appendChild(listItemEl);
 };
 
 formEl.addEventListener("submit", createTaskHandler);
